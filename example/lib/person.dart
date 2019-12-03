c21499794f5553a9dd131db22a8bcc23e56ac847
LINUX-XPS <jesusrope98@outlook.com>
2019-12-03 10:48:06 +0100

    Updated example

--- example/lib/person.dart
+++ example/lib/person.dart
@@ -1,6 +1,7 @@
+/// This is a very simple class, used to
+/// demo the `SearchPage` package
 class Person {
-  final String name;
-  final String surname;
+  final String name, surname;
   final num age;
 
   Person(this.name, this.surname, this.age);
