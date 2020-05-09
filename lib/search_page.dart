9509bac64f523cb45a4d7f1decacbb3dbdfb581b
Oliver Brunsmann <oliver.brunsmann@kernwerk.de>
2020-05-09 09:07:24 +0200

    Renames showItemsOnEmpty parameter

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -6,8 +6,10 @@ typedef ResultBuilder<T> = Widget Function(T t);
 /// This class helps to implement a search view, using [SearchDelegate].
 /// It can show suggestion & unsuccessful-search widgets.
 class SearchPage<T> extends SearchDelegate<T> {
-  /// Set this to true to display an empty list instead of the [suggestion].
-  final bool showListOnEmpty;
+  /// Set this to true to display the complete list instead of the [suggestion].
+  /// This is useful to give your users the chance to explore all the items in the
+  /// list without knowing what so search for.
+  final bool showItemsOnEmpty;
 
   /// Widget that is built when current query is empty.
   /// Suggests the user what's possible to do.
@@ -63,7 +65,7 @@ class SearchPage<T> extends SearchDelegate<T> {
     @required this.builder,
     @required this.filter,
     @required this.items,
-    this.showListOnEmpty = false,
+    this.showItemsOnEmpty = false,
     this.searchLabel,
     this.barTheme,
     this.itemStartsWith = false,
@@ -73,7 +75,7 @@ class SearchPage<T> extends SearchDelegate<T> {
         assert(builder != null),
         assert(filter != null),
         assert(items != null),
-        assert(showListOnEmpty != null),
+        assert(showItemsOnEmpty != null),
         super(searchFieldLabel: searchLabel);
 
   @override
@@ -158,7 +160,7 @@ class SearchPage<T> extends SearchDelegate<T> {
 
     // Builds a list with all filtered items
     // if query and result list are not empty
-    return cleanQuery.isEmpty && !showListOnEmpty
+    return cleanQuery.isEmpty && !showItemsOnEmpty
         ? suggestion
         : result.isEmpty
             ? failure
